f79e37f332d1a7f880eef3c117ca2722b157f556
dnallor1 <100855447+dnallor1@users.noreply.github.com>
2024-12-16 13:22:31 +0100

    Update main.py
    Changes & Improvements

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
 class Space():
-
     def __init__(self, height, width, num_hospitals):
         """Create a new state space with given dimensions."""
         self.height = height
@@ -18,7 +18,6 @@ class Space():
 
     def available_spaces(self):
         """Returns all cells not currently used by a house or hospital."""
-        # Consider all possible cells
         candidates = set(
             (row, col)
             for row in range(self.height)
@@ -35,67 +34,60 @@ class Space():
     def hill_climb(self, maximum=None, image_prefix=None, log=False):
         """Performs hill-climbing to find a solution."""
         count = 0
-        hospital_positions_per_iteration = [] 
+        hospital_positions_per_iteration = []
 
         # Start by initializing hospitals randomly
-        self.hospitals = set()
-        for i in range(self.num_hospitals):
-            self.hospitals.add(random.choice(list(self.available_spaces())))
+        self.hospitals = set(self.randomly_place_hospitals())
 
         if log:
             print("Initial state: cost", self.get_cost(self.hospitals))
 
-        # Continue until we reach maximum number of iterations
         while maximum is None or count < maximum:
             count += 1
-            best_neighbors = []
-            best_neighbor_cost = None
+            best_neighbors, best_neighbor_cost = self.find_best_neighbor()
 
-            # Consider all hospitals to move
-            for hospital in self.hospitals:
-
-                # Consider all neighbors for that hospital
-                for replacement in self.get_neighbors(*hospital):
-
-                    # Generate a neighboring set of hospitals
-                    neighbor = self.hospitals.copy()
-                    neighbor.remove(hospital)
-                    neighbor.add(replacement)
-
-                    # Check if neighbor is best so far
-                    cost = self.get_cost(neighbor)
-                    if best_neighbor_cost is None or cost < best_neighbor_cost:
-                        best_neighbor_cost = cost
-                        best_neighbors = [neighbor]
-                    elif best_neighbor_cost == cost:
-                        best_neighbors.append(neighbor)
-
-            # None of the neighbors are better than the current state
             if best_neighbor_cost >= self.get_cost(self.hospitals):
                 return self.hospitals, hospital_positions_per_iteration
 
-            # Move to a highest-valued neighbor
-            else:
-                if log:
-                    print(f"Found better neighbor: cost {best_neighbor_cost}")
-                self.hospitals = random.choice(best_neighbors)
+            self.hospitals = random.choice(best_neighbors)
+            if log:
+                print(f"Found better neighbor: cost {best_neighbor_cost}")
 
-            # Append after the first move and generate image
             hospital_positions_per_iteration.append(self.hospitals.copy())
-
-            # Generate image after this iteration
             if image_prefix:
                 self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
 
         return self.hospitals, hospital_positions_per_iteration
 
+    def randomly_place_hospitals(self):
+        """Randomly places hospitals in the available space."""
+        return [random.choice(list(self.available_spaces())) for _ in range(self.num_hospitals)]
+
+    def find_best_neighbor(self):
+        """Finds the best neighbor by evaluating all possible moves."""
+        best_neighbors = []
+        best_neighbor_cost = None
+
+        for hospital in self.hospitals:
+            for replacement in self.get_neighbors(*hospital):
+                neighbor = self.hospitals.copy()
+                neighbor.remove(hospital)
+                neighbor.add(replacement)
+
+                cost = self.get_cost(neighbor)
+                if best_neighbor_cost is None or cost < best_neighbor_cost:
+                    best_neighbor_cost = cost
+                    best_neighbors = [neighbor]
+                elif best_neighbor_cost == cost:
+                    best_neighbors.append(neighbor)
+
+        return best_neighbors, best_neighbor_cost
 
     def random_restart(self, maximum, image_prefix=None, log=False):
         """Repeats hill-climbing multiple times."""
         best_hospitals = None
         best_cost = None
 
-        # Repeat hill-climbing a fixed number of times
         for i in range(maximum):
             hospitals, _ = self.hill_climb()
             cost = self.get_cost(hospitals)
@@ -141,13 +133,11 @@ class Space():
 
     def output_image(self, filename):
         """Generates image with all houses and hospitals."""
-        from PIL import Image, ImageDraw, ImageFont
         cell_size = 100
         cell_border = 2
         cost_size = 40
         padding = 10
 
-        # Create a blank canvas
         img = Image.new(
             "RGBA",
             (self.width * cell_size,
@@ -165,8 +155,6 @@ class Space():
 
         for i in range(self.height):
             for j in range(self.width):
-
-                # Draw cell
                 rect = [
                     (j * cell_size + cell_border,
                      i * cell_size + cell_border),
@@ -180,7 +168,6 @@ class Space():
                 if (i, j) in self.hospitals:
                     img.paste(hospital, rect[0], hospital)
 
-        # Add cost
         draw.rectangle(
             (0, self.height * cell_size, self.width * cell_size,
              self.height * cell_size + cost_size + padding * 2),
@@ -203,6 +190,7 @@ class Space():
                 heatmap[row, col] = self.get_cost([(row, col)])
         return heatmap
 
+
 # Create a new space and add houses randomly
 s = Space(height=10, width=15, num_hospitals=1)
 for i in range(20):
@@ -213,11 +201,12 @@ hospitals, hospital_positions_per_iteration = s.hill_climb(maximum=200, image_pr
 
 heatmap = s.generate_heatmap()
 
+# Plotting heatmap
 plt.figure(1, figsize=(12, 6))
 plt.imshow(heatmap, cmap="hot", origin="lower")  
 plt.colorbar(label="Total Manhattan Distance")
 
-# # Plot houses and hospitals
+# Plot houses and hospitals
 plt.scatter([j for i, j in s.houses], [i for i, j in s.houses], color='blue', label="Houses", s=100)  
 plt.scatter([j for i, j in s.hospitals], [i for i, j in s.hospitals], color='green', marker='*', label="Hospitals", s=200)
 plt.grid(color='white', linestyle='--', linewidth=0.5, alpha=0.7)
@@ -237,8 +226,7 @@ for i in range(1, len(hospital_positions_per_iteration)):
     for prev_hospital, curr_hospital in zip(prev_hospitals, curr_hospitals):
         plt.plot([prev_hospital[1], curr_hospital[1]], [prev_hospital[0], curr_hospital[0]], color='green', linewidth=1.5, alpha=0.7)
 
-
-
+# 3D Heatmap
 fig = plt.figure(2, figsize=(12, 6))
 ax = fig.add_subplot(111, projection='3d')
 x, y = np.meshgrid(range(s.width), range(s.height))
@@ -246,26 +234,12 @@ z = heatmap
 surf = ax.plot_surface(x, y, z, cmap="hot", edgecolor="k", alpha=0.8)
 fig.colorbar(surf, label="Total Manhattan Distance", shrink=0.5, aspect=10)
 ax.scatter([j for i, j in s.houses], [i for i, j in s.houses], [0] * len(s.houses), color='blue', label="Houses", s=50)
-ax.scatter([j for i, j in s.hospitals], [i for i, j in s.hospitals], [z[int(i), int(j)] for i, j in s.hospitals], color='green', label="Hospitals", s=100)
-ax.set_title("3D Heat Map of Manhattan distances")
+ax.scatter([j for i, j in s.hospitals], [i for i, j in s.hospitals], [z[int(i), int(j)] for i, j in s.hospitals], color='green', marker='*', label="Hospitals", s=100)
+
 ax.set_xlabel("X-Axis")
 ax.set_ylabel("Y-Axis")
-ax.set_zlabel("Cost (Z)")
-ax.view_init(elev=30, azim=45)  # Adjust view angle
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-# Draw ladder paths from hospital positions across iterations
-for i in range(1, len(hospital_positions_per_iteration)):
-    prev_hospitals = hospital_positions_per_iteration[i - 1]
-    curr_hospitals = hospital_positions_per_iteration[i]
-    for prev_hospital, curr_hospital in zip(prev_hospitals, curr_hospitals):
-        ax.plot([prev_hospital[1], curr_hospital[1]], 
-                [prev_hospital[0], curr_hospital[0]], 
-                [z[int(prev_hospital[0]), int(prev_hospital[1])], 
-                 z[int(curr_hospital[0]), int(curr_hospital[1])]],
-                color='green', linewidth=1.5, alpha=0.7)
-
+ax.set_zlabel("Z-Axis (Cost)")
+ax.set_title("3D Heatmap with Hospital Paths")
+ax.legend()
 
 plt.show()
